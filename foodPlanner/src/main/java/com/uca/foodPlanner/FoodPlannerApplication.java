8962dc8915b80ff1191d1434b9af48bfe12389ba
Daniel Cabrera <dcabrera@atsistemas.com>
2020-03-14 16:03:43 +0100

    feat:  Maven project creation

---
+++ foodPlanner/src/main/java/com/uca/foodPlanner/FoodPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.uca.foodPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodPlannerApplication.class, args);
+	}
+
+}
---
+++ foodPlanner/src/test/java/com/uca/foodPlanner/FoodPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.foodPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
